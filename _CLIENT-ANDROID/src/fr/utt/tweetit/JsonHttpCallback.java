d1187d8aa56f62bafb93a4c484c9450b84131de3
steven salaun <steven.salaun@gmail.com>
2013-11-07 15:57:33 +0100

    reset client

--- _CLIENT-ANDROID/src/fr/utt/tweetit/JsonHttpCallback.java
+++
@@ -1,7 +0,0 @@
-package fr.utt.tweetit;
-
-import org.json.JSONObject;
-
-public interface JsonHttpCallback {
-	public Object call(JSONObject jsonResponse);
-}
--- _CLIENT-ANDROID/src/fr/utt/tweetit/OnFragmentInteractionListener.java
+++
@@ -1,7 +0,0 @@
-package fr.utt.tweetit;
-
-import android.net.Uri;
-
-public interface OnFragmentInteractionListener {
-	public void onFragmentInteraction(Uri uri);
-}
